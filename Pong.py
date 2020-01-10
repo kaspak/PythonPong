4be6e57c0d00920bc9aeba1c71ebf809898b25ed
Kassy Pak <kpak3@dons.usfca.edu>
2020-01-10 15:34:28 -0800

    Created Paddles A, B, and the pong ball. Also created keyboard to listen to IO, and made borders to prevent ball and paddles to move off screen.

---
+++ Pong.py
@@ -0,0 +1,115 @@
+#Simple Pong in Python 3 for Beginners
+# Tutorial by @TokyoEdTech, Author: Kassy Pak
+
+import turtle
+
+window = turtle.Screen()
+window.title("Pong by Kassy Pak")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+# Paddle A ---------------------------------------
+pad_A = turtle.Turtle()
+
+# sets paddle to possible maximum speed
+pad_A.speed(0)
+pad_A.shape("square")
+pad_A.color("white")
+pad_A.shapesize(stretch_wid=5, stretch_len=1)
+pad_A.penup()
+
+# sets paddle to left starting position
+pad_A.goto(-350, 0)
+
+
+# Paddle B ---------------------------------------
+pad_B = turtle.Turtle()
+pad_B.speed(0)
+pad_B.shape("square")
+pad_B.color("white")
+pad_B.shapesize(stretch_wid=5, stretch_len=1)
+pad_B.penup()
+
+# sets paddle to right starting position
+pad_B.goto(350, 0)
+
+
+# Ball -------------------------------------------
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+
+# sets paddle to center starting position
+ball.goto(0, 0)
+
+# Moves balls by two pixels
+ball.dx = 2
+ball.dy = -2
+
+
+# Function ---------------------------------------
+def pad_A_up():  # Need to know the current y-coordinate to move paddle
+    currentA_y = pad_A.ycor()
+    currentA_y += 20
+    pad_A.sety(currentA_y)
+
+
+def pad_A_down():
+    currentA_y = pad_A.ycor()
+    currentA_y -= 20
+    pad_A.sety(currentA_y)
+
+
+def pad_B_up():  # Need to know the current y-coordinate to move paddle
+    currentB_y = pad_B.ycor()
+    currentB_y += 20
+    pad_B.sety(currentB_y)
+
+
+def pad_B_down():
+    currentB_y = pad_B.ycor()
+    currentB_y -= 20
+    pad_B.sety(currentB_y)
+
+# Keybord Binding --------------------------------
+window.listen()  # Takes keyboard input
+window.onkeypress(pad_A_up, "w")
+window.onkeypress(pad_A_down, "s")
+window.onkeypress(pad_B_up, "Up")
+window.onkeypress(pad_B_down, "Down")
+
+
+# Main game loop ---------------------------------
+while True:
+    window.update()
+
+    # Moving the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1  # Reverses the direction of ball
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1  # Reverses the direction of ball
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350 ) and (ball.ycor() < pad_B.ycor() + 40 and ball.ycor() > pad_B.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
